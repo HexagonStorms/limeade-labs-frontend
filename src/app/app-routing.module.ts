59a723134328ac2045391e5d8980d8bca40c2bae
Josue Plaza <plazajosue2@gmail.com>
2018-08-04 21:03:19 -0500

    routing complete, updated footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { OurTeamComponent } from './our-team/our-team.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FaqComponent } from './faq/faq.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "services", component: ServicesComponent },
   { path: "our-team", component: OurTeamComponent },
+  { path: "portfolio", component: PortfolioComponent },
+  { path: "faq", component: FaqComponent },
+  { path: "testimonial", component: TestimonialComponent },
   // DEFAULT PATH
   { path: "", redirectTo: "/", pathMatch: "full"}
  ];
