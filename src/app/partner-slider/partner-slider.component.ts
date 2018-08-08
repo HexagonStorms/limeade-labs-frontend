047d83f3110a6094ad10490ef6b308f2e4b4cb79
Josue Plaza <plazajosue2@gmail.com>
2018-08-08 14:39:50 -0500

    deploying

--- src/app/partner-slider/partner-slider.component.ts
+++ src/app/partner-slider/partner-slider.component.ts
@@ -6,6 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partner-slider.component.css']
 })
 export class PartnerSliderComponent implements OnInit {
+  defaultPath = "./assets/images/logos/";
+  partners = [
+    {
+      name: "Bravelets",
+      image: "bravelets.png"
+    },
+    {
+      name: "Clearhead",
+      image: "clearhead-logo.png"
+    },
+    {
+      name: "Texas Department of Transportation",
+      image: "dps-logo-bw.png"
+    },
+    {
+      name: "San Antonio Express News",
+      image: "saen-logo.png"
+    },
+    {
+      name: "El Buen",
+      image: "el-buen-logo.png"
+    },
+    {
+      name: "Integral Care",
+      image: "integral-care-logo.png"
+    },
+    {
+      name: "Mozido",
+      image: "mozido-logo.png"
+    },
+    {
+      name: "Parlevel Systems",
+      image: "parlevel.png"
+    },
+    {
+      name: "Talos",
+      image: "talos-logo.jpg"
+    }
+  ];
 
   constructor() { }
 
