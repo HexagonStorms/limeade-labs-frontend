2dbc4177d47de3c3d815bdfec1693b96de88e751
Josue Plaza <plazajosue2@gmail.com>
2018-08-08 19:19:56 -0500

    our team page done

--- src/app/our-team/our-team.component.ts
+++ src/app/our-team/our-team.component.ts
@@ -12,4 +12,37 @@ export class OurTeamComponent implements OnInit {
   ngOnInit() {
   }
 
+  staff = [
+    {
+      name: "Josue Plaza",
+      title: "Lead Developer",
+      image: "./assets/images/team/josue_500x667.gif",
+      quote: "The way we spend our days is the way we spend our lives."
+    },
+    {
+      name: "Joshua Garza",
+      title: "Lead Designer",
+      image: "./assets/images/team/josh_500x667.gif",
+      quote: "I love women! Womenwomenwomenwomen."
+    },
+    {
+      name: "Michael Famitafreshi",
+      title: "Head of Finance",
+      image: "./assets/images/team/michael2_500x667.gif",
+      quote: "We are moving the RGV forward, one App at a time."
+    },
+    {
+      name: "Victor Rodriguez",
+      title: "Full Stack Developer",
+      image: "./assets/images/team/victor2_500x667.gif",
+      quote: "The best preparation for tomorrow is doing your best today."
+    },
+    {
+      name: "Roxanne Garcia",
+      title: "Full Stack Developer",
+      image: "./assets/images/team/roxanne2_500x667.gif",
+      quote: "I pick Mario in Super Smash. Enough said."
+    }
+  ];
+
 }
